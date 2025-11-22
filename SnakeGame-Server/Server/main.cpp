ec45438db909bdd63172aeeddeb2b375cead434a
Seongyeol Yun <syyundev@gmail.com>
2025-11-23 00:47:10 +0900

    BroadcastThread 삭제

--- SnakeGame-Server/Server/main.cpp
+++ SnakeGame-Server/Server/main.cpp
@@ -2,7 +2,7 @@
 #include "ServerManager.h"
 #include "GameMap.h"
 
-// 메인쓰레드
+// MainThread
 // - AcceptThread: accept만 담당하는 쓰레드
 //	 - Client Thread 1
 //	 - Client Thread 2
@@ -10,7 +10,6 @@
 //	 - ....
 //	 - Client Thread N
 // - gameThread: 충돌판정, 먹이 생성한다.
-// - broadcastThread: 
 
 int main()
 {
@@ -22,7 +21,6 @@ int main()
 
 		std::jthread acceptThread{ [](const std::stop_token& st) { MANAGER(ServerManager)->DoAccept(st); } };
 		std::jthread gameThread([](const std::stop_token& st) { MANAGER(GameMap)->Update(st); });
-		// std::jthread broadcastThread([](const std::stop_token& st) { MANAGER(ServerManager)->FlushSendBufferQueue(st); });
 		
 		// 메인쓰레드
 		std::string word;
