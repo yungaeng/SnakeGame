cb926b8879dc61faa757ec2baa41206e21424c29
Seongyeol Yun <syyundev@gmail.com>
2025-11-29 00:16:17 +0900

    리드미 수정

--- SnakeGame-Server/Server/main.cpp
+++ SnakeGame-Server/Server/main.cpp
@@ -2,15 +2,6 @@
 #include "ServerManager.h"
 #include "GameMap.h"
 
-// MainThread
-// - AcceptThread: accept만 담당하는 쓰레드
-//	 - Client Thread 1
-//	 - Client Thread 2
-//   - Client Thread 3
-//	 - ....
-//	 - Client Thread N
-// - gameThread: 충돌판정, 먹이 생성한다.
-
 int main()
 {
 	ClientPacketHandler::Init();
